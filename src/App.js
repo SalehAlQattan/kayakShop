4049f2e936727c0b3b0d6f059ff0c2bd8a318f04
Saleh AlQattan <salehalqattan@outlook.com>
2021-06-07 18:03:16 +0300

    rename the folder, move image folder to src folder, rename the website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import kayak from './images/kayak.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Kayak Shop</h1>
       <p>Buy your Kayak now!</p>
-      <img className="kayak-img" src="images/kayak.jpg" />
+      <img className="kayak-img" src={kayak} />
     </div>
   );
 }
