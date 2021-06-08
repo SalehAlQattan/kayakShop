1078948add859b81e56fa2c08875532bb73ffb2f
Saleh AlQattan <salehalqattan@outlook.com>
2021-06-08 08:10:23 +0300

    mapping over the kayak list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import kayak from './images/kayak.jpg';
+import HomePagekayak from './images/HomePagekayak.jpg';
+import kayaks from './products';
 
 function App() {
+  const kayakList = kayaks.map(kayak => {
+    return (
+      <div className="product-container">
+        <img className="product-img" src={kayak.img} alt={kayak.name} />
+        <h2 className="product-title">{kayak.name}</h2>
+        <h3 className="product-price">{kayak.price} KD</h3>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-      <h1>Welcome to the Kayak Shop</h1>
-      <p>Buy your Kayak now!</p>
-      <img className="kayak-img" src={kayak} />
+      <h1 className="title">Welcome to the Kayak Shop</h1>
+      <h2 className="sub-title">Buy your Kayak now!</h2>
+      <img className="kayak-img" src={HomePagekayak} alt="kayak img" />
+      <div className="kayaks">{kayakList}</div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,28 @@
+// import singleKayak from './images/singleKayak.jpeg';
+// import doubleKayak from './images/doubleKayak.jpg';
+// import transparentKayak from './images/transparentKayak.jpeg';
+
+const kayaks = [
+  {
+    name: 'Single Kayak',
+    price: 120,
+    img: 'https://cdn.shopify.com/s/files/1/0086/9128/6076/products/z_47_0_0_258b232e-aca4-4c22-afe7-d68d1e988582.jpg?v=1607358036',
+  },
+  {
+    name: 'Double Kayak',
+    price: 140,
+    img: 'https://www.wavepassion.co.nz/pub/media/catalog/product/cache/94ce616ad4b467ecaa896781a450eceb/e/z/ez365-orange-05_1.jpg',
+  },
+  {
+    name: 'Transparent Kayak',
+    price: 180,
+    img: 'https://mk0myhipblogcxgamt0p.kinstacdn.com/wp-content/uploads/2016/08/molokini-clear-blue-at-aghalife-original-800x600.jpg',
+  },
+  {
+    name: 'Inflatable Kayak',
+    price: 100,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71Gix6EC8hL._AC_SX522_.jpg',
+  },
+];
+
+export default kayaks;
